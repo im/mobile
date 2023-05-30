35d2b0bb78783b7bfc9f3d78cec6fe069b4a559b
im <im@users.noreply.github.com>
2023-05-30 08:54:52 +0000

    deploy: f3dbe9f675fc0ea5fb2a6581de317c917ee516a1

--- assets/index-259c35f9.js
+++
@@ -1,1 +0,0 @@
-import{d as s,h as _,o,p as t,f as c,g as a}from"./index-4883542a.js";import{_ as d}from"./_plugin-vue_export-helper-c27b6911.js";const p=e=>(t("data-v-0bfdc582"),e=e(),c(),e),n=p(()=>a("h1",null,"消息通知",-1)),r=[n],f=s({__name:"message",setup(e){return(l,m)=>(o(),_("div",null,r))}});const h=d(f,[["__scopeId","data-v-0bfdc582"]]);export{h as default};
